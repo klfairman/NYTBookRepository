2d42fdc6cd596097c9808958928491cd7fcf2303
Kristen Fairman <fairman.kristen@gmail.com>
2018-12-03 18:45:25 -0600

    final

---
+++ config/key.js
@@ -0,0 +1,2 @@
+module.exports =
+  "mongodb://heroku_w7125zml:heroku_w7125zml@ds259711.mlab.com:59711/heroku_w7125zml";
--- js/config.js
+++
@@ -1,5 +0,0 @@
-const config = {
-    NYT_KEY : '54cff2ccc41041daa6f1df356be8fa17', // NYTimes Key,
-    GOOGLE_BOOKS_KEY : 'AIzaSyDaFK_T9jyOxPe8-7aDhCu7ztJxSxW4K5g', // Google Books Key
-    TASTEDIVE_KEY: '310838-TopBRec-96B8OD4S'
-  };
